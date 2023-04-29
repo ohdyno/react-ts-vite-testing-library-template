81f3d69b8183250c6a5f815ddd97ed217c2268b1
Xing Zhou <xing@xingzhou.me>
2023-04-28 22:10:29 -0400

    feat: create project with React, Vite, Vitest, and Testing-Library

---
+++ __tests__/App.test.tsx
@@ -0,0 +1,11 @@
+import {render, screen} from '@testing-library/react';
+
+import App from '../src/App';
+
+describe('App', () => {
+    it('renders headline', () => {
+        render(<App/>);
+
+        expect(screen.getByText('Vite + React')).toBeInTheDocument()
+    });
+});
