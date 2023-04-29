b3d9a1e7c0231c0865ffbd003a1b72f5e4469916
Xing Zhou <xing@xingzhou.me>
2023-04-28 22:38:16 -0400

    feat: use 'user-event' from Testing Library in tests

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,11 +1,17 @@
 import {render, screen} from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
 
 import App from '../src/App';
 
 describe('App', () => {
-    it('renders headline', () => {
+    it('renders headline', async () => {
+        const user = userEvent.setup()
         render(<App/>);
 
-        expect(screen.getByText('Vite + React')).toBeInTheDocument()
+        const countButton = screen.getByText('count is 0');
+        expect(countButton).toBeInTheDocument()
+
+        await user.click(countButton)
+        expect(screen.getByText(/count is 1/)).toBeInTheDocument()
     });
 });
